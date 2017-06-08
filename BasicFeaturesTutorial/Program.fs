2664e4611e37a2aaf58fb17c114797ff6742653c
Kevin Avignon <kavignon@ludia.com>
2017-06-08 17:59:23 -0400

    Added a bullet point formatter that uses core features from F#

--- BasicFeaturesTutorial/Program.fs
+++ BasicFeaturesTutorial/Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 open TypeProvider
+open BulletPointFormatter 
 
 [<EntryPoint>]
 let main argv = 
@@ -13,6 +14,17 @@ let main argv =
     for bd in BankData.Array do 
         printfn "%s" bd.Country.Value
         printfn "%d" bd.Date
+
+    let nList = 
+        [   { Line = { Weight = HW1; Text = "All About Birds" } } 
+            { Line ={ Weight = HW2; Text = "Kinds of Birds" } } 
+            { Line ={ Weight = HW3; Text = "The Finch" } } 
+            { Line ={ Weight = HW3; Text = "The Swan" } }
+            { Line ={ Weight = HW2; Text = "The habitats" } }
+            { Line ={ Weight = HW3; Text = "Wetland" } }
+            { Line ={ Weight = HW3; Text = "Sky" } } ]
+    let outline = formatTextOutline nList
+    printf "%s" outline.Text
 
     printfn "%A" argv
     0 // return an integer exit code
