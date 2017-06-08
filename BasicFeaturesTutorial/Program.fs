3f3d379c7e58343dafe5d8017a04682a70203174
Kevin Avignon <kavignon@ludia.com>
2017-06-08 15:16:39 -0400

    No content was found and problem is fixed.

--- BasicFeaturesTutorial/Program.fs
+++ BasicFeaturesTutorial/Program.fs
@@ -1,7 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open TypeProvider
 
 [<EntryPoint>]
 let main argv = 
+
+    for item in People.GetSamples() do 
+      printf "%s " item.Name 
+      item.Age |> Option.iter (printf "(%d)")
+      printfn ""
+
+    for bd in BankData.Array do 
+        printfn "%s" bd.Country.Value
+        printfn "%d" bd.Date
+
     printfn "%A" argv
     0 // return an integer exit code
---
+++ BasicFeaturesTutorial/TypeProvider.fs
@@ -0,0 +1,21 @@
+﻿module TypeProvider
+open FSharp.Data
+
+type People = JsonProvider<""" 
+  [ { "name":"John", "age":94 }, 
+    { "name":"Tomas" } ] """>
+
+for item in People.GetSamples() do 
+  printf "%s " item.Name 
+  item.Age |> Option.iter (printf "(%d)")
+  printfn ""
+
+
+type WorldBank = JsonProvider<"""[ { "page": 1, "pages": 1, "total": 53 },
+  [ { "indicator": {"value": "Central government debt, total (% of GDP)"},
+      "country": {"id":"CZ","value":"Czech Republic"},
+      "value":null,"decimal":"1","date":"2000"},
+    { "indicator": {"value": "Central government debt, total (% of GDP)"},
+      "country": {"id":"CAN","value":"Canada"},
+      "value":"16.6567773464055","decimal":"1","date":"2010"} ] ]""">
+let BankData = WorldBank.GetSample()
